e7139840e50b6e0b894ab449124af8c83eefee91
Elijah Smith <elijah.d.s@outlook.com>
2023-10-03 21:09:24 -0400

    Adding the Users Controller

---
+++ MVCSutoGameStudio.App/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using CRUDApps.DataAccess.EF.Context;
+using MVCSutoGameStudioAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using CRUDApps.DataAccess.EF.Models;
+
+namespace MVCSutoGameStudioAPP.Controllers
+{
+	public class UsersController : Controller
+	{
+		private readonly SutoGameStudioContext _Context;
+
+		public UsersController(SutoGameStudioContext context)
+		{
+			_Context = context;
+		}
+
+		public IActionResult Index()
+		{
+			UsersViewModel model = new UsersViewModel(_Context);
+			return View(model);
+		}
+
+		[HttpPost]
+		public IActionResult Index(int userId, string userName, string customerFirstName, string customerLastName, string customerEmail, string customerState, LoyaltyCharts userNameNavigation)
+		{
+			UsersViewModel model = new UsersViewModel(_Context);
+			Users users = new(userId, userName, customerFirstName, customerLastName, customerEmail, customerState, userNameNavigation);
+
+			model.SaveUsers(users);
+			model.IsActionSuccess = true;
+			model.ActionMessage = "New User has been added to the database.";
+
+			return View(model);
+		}
+
+		public IActionResult Update(int id)
+		{
+			UsersViewModel model = new UsersViewModel(_context, id);
+			return View(model);
+		}
+
+		public IActionResult Delete(int id)
+		{
+			UsersViewModel model = new UsersViewModel(_Context);
+
+			if (id > 0)
+			{
+				model.RemoveUsers(id);
+			}
+
+			model.IsActionSuccess = true;
+			model.ActionMessage = "The user has been successfully removed from the database.";
+			return View("Index", model);
+		}
+	}
+}
