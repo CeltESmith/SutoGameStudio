57ddc197cffc1756e1721d36250c98c19bb44ddd
Elijah Smith <elijah.d.s@outlook.com>
2023-10-04 21:16:17 -0400

    Adding model classes

---
+++ MVCSutoGameStudio.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSutoGameStudio.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
