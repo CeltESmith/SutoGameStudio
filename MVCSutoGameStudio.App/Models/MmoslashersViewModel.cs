8319f0a43e96bce012ac5f055c442191368ba369
Elijah Smith <elijah.d.s@outlook.com>
2023-10-04 21:17:10 -0400

    Adding model classes

---
+++ MVCSutoGameStudio.App/Models/MmoslashersViewModel.cs
@@ -0,0 +1,70 @@
+﻿using CRUDApps.DataAccess.EF.Repositories;
+using CRUDApps.DataAccess.EF.Models;
+using CRUDApps.DataAccess.EF.Context;
+
+namespace MVCSutoGameStudioAPP.Models
+{
+	public class MmoslashersViewModel
+	{
+		private readonly MmoslashersRepository _repo;
+
+		public List<Mmoslashers> MmoslashersList { get; set; }
+		public Mmoslashers CurrentMmoslashers { get; set; }
+		public bool IsActionSuccess { get; set; }
+		public string ActionMessage { get; set; }
+
+		public MmoslashersViewModel(SutoGameStudioContext context)
+		{
+			_repo = new MmoslashersRepository(context);
+			MmoslashersList = GetAllMmoslashers();
+			CurrentMmoslashers = MmoslashersList.FirstOrDefault();
+		}
+
+		public MmoslashersViewModel(SutoGameStudioContext context, int mmoslasherId)
+		{
+			_repo = new MmoslashersRepository(context);
+			MmoslashersList = GetAllMmoslashers();
+
+			if (mmoslasherId > 0 )
+			{
+				CurrentMmoslashers = GetMmoslashersByID(mmoslasherId);
+			}
+			else
+			{
+				CurrentMmoslashers = new Mmoslashers();
+			}
+		}
+
+		public void SaveMmoslashers(Mmoslashers mmoslashers)
+		{
+			if (mmoslashers.MmoslasherId > 0)
+			{
+				_repo.update(mmoslashers);
+			}
+			else
+			{
+				mmoslashers.MmoslasherId = _repo.Create(mmoslashers);
+			}
+
+			MmoslashersList = GetAllMmoslashers();
+			CurrentMmoslashers = GetMmoslashersByID(mmoslashers.MmoslasherId);
+		}
+
+		public void RemoveMmoslashers(int mmoslasherId)
+		{
+			_repo.Delete(mmoslasherId);
+			MmoslashersList = GetAllMmoslashers();
+			CurrentMmoslashers = MmoslashersList.FirstOrDefault();
+		}
+
+		public List<Mmoslashers> GetAllMmoslashers()
+		{
+			return _repo.GetAllMmoslashers();
+		}
+
+		public Mmoslashers GetMmoslashersByID(int mmoslasherID)
+		{
+			return _repo.GetMmoslashersByID(mmoslasherID);
+		}
+	}
+}
