3e9106bc5f9963ada30f3cae0c4e3e255ac792b8
Elijah Smith <elijah.d.s@outlook.com>
2023-10-04 21:18:02 -0400

    Adding model classes

---
+++ MVCSutoGameStudio.App/Models/UsersViewModel.cs
@@ -0,0 +1,69 @@
+﻿using CRUDApps.DataAccess.EF.Context;
+using CRUDApps.DataAccess.EF.Models;
+using CRUDApps.DataAccess.EF.Repositories;
+
+namespace MVCSutoGameStudioAPP.Models
+{
+	public class UsersViewModel
+	{
+		private readonly UsersRepository _repo;
+
+		public List<Users> UsersList { get; set; }
+		public Users CurrentUsers { get; set; }
+		public bool IsActionSuccess { get; set; }
+		public string ActionMessage { get; set; }
+
+		public UsersViewModel(SutoGameStudioContext context)
+		{
+			_repo = new UsersRepository(context);
+			UsersList = GetAllUsers();
+			CurrentUsers = UsersList.FirstOrDefault();
+		}
+
+		public UsersViewModel(SutoGameStudioContext context, int userId)
+		{
+			_repo = new UsersRepository(context);
+
+			if (userId > 0 )
+			{
+				CurrentUsers = GetUsersByID(userId);
+			}
+			else
+			{
+				CurrentUsers = new Users();
+			}
+		}
+
+		public void SaveUSers(Users users)
+		{
+			if (users.UserId > 0 )
+			{
+				_repo.Update(users);
+			}
+			else
+			{
+				users.UserId = _repo.Create(users);
+			}
+
+			UsersList = GetAllUsers();
+			CurrentUsers = GetUsersByID(users.UserId);
+		}
+
+		public void RemoveUsers(int userId)
+		{
+			_repo.Delete(userId);
+			UsersList = GetAllUsers();
+			CurrentUsers = UsersList.FirstOrDefault();
+		}
+
+		public List<Users> GetAllUsers()
+		{
+			return _repo.GetAllUsers();
+		}
+
+		public Users GetUsersByID(int userID)
+		{
+			return _repo.GetUsersByID(userID);
+		}
+	}
+}
