3f49cd1dc413d64233c801996ea89e70850d2d4a
Elijah Smith <elijah.d.s@outlook.com>
2023-10-11 22:09:47 -0400

    FPS blasts View Cshtml classes

---
+++ MVCSutoGameStudio.App/Views/Fpsblasts/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCSutoGameStudio.App.Views.Fpsblasts
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MVCSutoGameStudio.App/Views/Fpsblasts/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCSutoGameStudio.App.Views.Fpsblasts
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
