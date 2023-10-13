b6b0131ba34f0fc7e61a5577e3d5498a8fed0453
Elijah Smith <elijah.d.s@outlook.com>
2023-10-13 14:42:13 -0400

    Updates to Indexs

---
+++ MVCSutoGameStudio.App/Views/RPGstoryMakers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCSutoGameStudio.App.Views.RPGstoryMakers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MVCSutoGameStudio.App/Views/RPGstoryMakers/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCSutoGameStudio.App.Views.RPGstoryMakers
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
